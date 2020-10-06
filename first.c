b200fc75808c04d3b86986f3cc41383f12bfb56b
NicAir <60952091+NicAir@users.noreply.github.com>
2020-10-06 18:49:50 +0800

    Update first.c
    第一次使用GitHub

--- first.c
+++ first.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-int main(void)
+#include<stdio.h>           /*一个简单的C程序*/
+int main(void)    
 {
-  int num;
-  num = 1;
-  
-  printf("I am a simple ");
+  int num;                  /*定义一个名为num的变量*/
+  num = 1;                  /*为num附一个值*/
+        
+  printf("I am a simple "); /*使用printf()函数*/
   printf("computer.\n");
   printf("My favorite number is %d because it is first.\n",num);
   
